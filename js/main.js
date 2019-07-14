b0ca2b619a6b8fe879236f95a80f2591e2773eb3
vuhoanglinh <vuhoanglinh2002@gmail.com>
2019-07-15 00:45:38 +0700

    add parallax style

--- js/main.js
+++ js/main.js
@@ -2,34 +2,66 @@
 * Smooth Scrolling Navigation
 */
 jQuery(document).ready(function($) {
-    // Scroll to the desired section on click
-    // Make sure to add the `data-scroll` attribute to your `<a>` tag.
-    // Example: 
-    // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
-    function scrollTo(event) {
-      event.preventDefault();
-      var selector = $(this).attr('href'); 
-      history.pushState(null, null, selector);
-      scrollToSection(selector);
+  // Scroll to the desired section on click
+  // Make sure to add the `data-scroll` attribute to your `<a>` tag.
+  // Example: 
+  // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
+  function scrollTo(event) {
+    event.preventDefault();
+    var selector = $(this).attr('href'); 
+    history.pushState(null, null, selector);
+    scrollToSection(selector);
+  }
+  $('[data-scroll]').on('touchstart click', scrollTo);
+  scrollToTop('.back-to-top');
+  slickCarousel('[data-slick]');
+  scrollToSection(location.hash);
+  // $(window).on( 'hashchange', function( e ){
+  //   event.preventDefault();
+  //   scrollToSection(location.hash);
+  // });
+
+  /**
+  * Parallax Scrolling
+  */
+  jQuery(document).ready(function($) {
+    if(jQuery().paroller) {
+      $('[data-parallax]').paroller();
     }
-    $('[data-scroll]').on('click', scrollTo);
-    scrollToTop('.back-to-top');
-    slickCarousel('[data-slick]');
-    scrollToSection(location.hash);
-    // $(window).on( 'hashchange', function( e ){
-    //   event.preventDefault();
-    //   scrollToSection(location.hash);
-    // });
+  });
+
+  $(window).scroll(function() {
+    var scrolled = $(window).scrollTop()
+    $('.parallax').each(function(index, element) {
+      var initY = $(this).offset().top
+      var height = $(this).height()
+      var endY  = initY + $(this).height()
+      var size = parseInt($(this).attr('data-ratio') || 20);
+  
+      // Check if the element is in the viewport.
+      var visible = isInViewport(this)
+      if(visible) {
+        var diff = scrolled - initY
+        var ratio = Math.round((diff / height) * size);
+        $(this).css('transform', 'translateY(' + parseInt(-(ratio * 1.5)) + 'px)');
+      }
+    })
+  })
+
 }(jQuery));
 
 function scrollToSection(selector) {
-  var section = $(selector);
-  if (section) {
-    $('html, body').animate({
-      scrollTop: section.offset().top
-    }, 500);
-    $('.navbar-toggler').addClass('collapsed');
-    $('.navbar-collapse').removeClass('show');
+  try {
+    var section = $(selector);
+    if (section) {
+      $('html, body').animate({
+        scrollTop: section.offset().top
+      }, 500);
+      $('.navbar-toggler').addClass('collapsed');
+      $('.navbar-collapse').removeClass('show');
+    }
+  } catch (ex) {
+    console.log(ex);
   }
 }
 
@@ -87,4 +119,15 @@ function slickCarousel(selector) {
       // instead of a settings object
     ]
   });
+}
+
+function isInViewport(node) {
+  var rect = node.getBoundingClientRect()
+  return (
+    (rect.height > 0 || rect.width > 0) &&
+    rect.bottom >= 0 &&
+    rect.right >= 0 &&
+    rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+  )
 }
